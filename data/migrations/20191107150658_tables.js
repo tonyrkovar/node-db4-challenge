9e521deb343d6490cf742e95f0341222d56cd03e
Tony <tonyrkovar@gmail.com>
2019-11-07 15:28:54 -0600

    about to test the migration I wrote out and add seed data

--- data/migrations/20191107150658_tables.js
+++ data/migrations/20191107150658_tables.js
@@ -1,8 +1,21 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knes.schema.createTable('recipies', tbl => {
+        tbl.increments();
+        tbl.string('recipe_name', 255).notNullable()
+        tbl.string('ingredients').unsigned().references('name').inTable('ingredients').onDelete('RESTRICT').onUpdate('CASCADE').notNullable()
+        tbl.float('quantity').notNullable()
+    })
+        .createTable('ingredients', tbl => {
+            tbl.increments();
+            tbl.string('ingredient_name', 128).notNullable();
+        })
+        .createTable('instructions', tbl => {
+            tbl.increments();
+            tbl.string('steps', 255)
+        })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+
 };
